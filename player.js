d182f48fd43ca9696308355ab4d22684294c5996
Julien Monchany <julien.monchany@gmail.com>
2014-01-07 12:12:52 -0800

    linkiing library to Jplayer, Jplaylist implementation, player design improved

--- player.js
+++ player.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var app = express();
 
-var id3 = require('id3js');
-
 var fs = require('fs');
 var path = require('path');
 
-var musicdir = "/home/jumon/music";
+var musicdir = "/home/pi/music";
 
 var walk = function(dir, done) {
   var results = [];
@@ -25,14 +23,6 @@ var walk = function(dir, done) {
           });
         } else {
           results.push(file);
-          /*id3({ file: "/home/pi/music/Avicii/12 Edom.mp3", type: id3.OPEN_LOCAL }, function(err, tags) {
-                //console.log("adding "+ file);
-                if (err) throw err;
-                results = results.concat(tags);
-                done(null, results);
-                //console.log(tags);
-          }); */
-          console.log(results);
           if (!--pending) done(null, results);
         }
       });
@@ -40,23 +30,17 @@ var walk = function(dir, done) {
   });
 };
 
-
-
-
 // Express config
 app.engine('.html', require('ejs').__express);
 app.set('view engine', 'html');
 app.use("/public",express.static(__dirname + "/public"));
-// app.use("/styles",express.static(__dirname + "/styles"));
-// app.use("/js",express.static(__dirname + "/js"));
 app.use("/home",express.static("/home"));
 
 app.get('/', function(req, res) {
 	// read content of the music directory
 	walk(musicdir, function(err, results) {
 		if (err) throw err;
-    console.log(results);
-		res.render('index',{playlist: results});
+		res.render('index',{library: results});
 	});
 });
 
