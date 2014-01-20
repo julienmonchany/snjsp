309a3463422e47e0ac489629fdf9691eb5d6811d
Julien Monchany <julien.monchany@gmail.com>
2014-01-20 05:13:58 -0800

    minor updates

--- public/js/script.js
+++ public/js/script.js
@@ -1,28 +1,31 @@
 $(document).ready(function() {
-    
-	// add the selected song to the playlist
-        $(document.body).on('click','#library .song',function(){
-		myPlaylist.add({
-                        // To fix
-			title:$(this).text(),
-			mp3: $(this).attr('value')
-		}, false);
-	 });
 
-	var myPlaylist = new jPlayerPlaylist({
-		jPlayer: "#jquery_jplayer_1",
-		cssSelectorAncestor: "#jp_container_1"
-	}, 
-	[], 
-	{
-		playlistOptions: {
-			enableRemoveControls: true,
-			addTime: 0
-		},
+    // add the selected song to the playlist
+    $(document.body).on('click', '#library .song', function() {
+        myPlaylist.add({
+            // To fix
+            title: $(this).text(),
+            mp3: $(this).attr('value')
+        }, false);
+    });
+
+
+
+
+    var myPlaylist = new jPlayerPlaylist({
+        jPlayer: "#jquery_jplayer_1",
+        cssSelectorAncestor: "#jp_container_1"
+    },
+    [],
+            {
+                playlistOptions: {
+                    enableRemoveControls: true,
+                    addTime: 0
+                },
 //			swfPath: "/js",
-			supplied: "mp3, flac",
-			smoothPlayBar: true,
-			keyEnabled: true
-	});
+                supplied: "mp3, flac, m4a, webma, oga",
+                smoothPlayBar: true,
+                keyEnabled: true
+    });
 
 });
